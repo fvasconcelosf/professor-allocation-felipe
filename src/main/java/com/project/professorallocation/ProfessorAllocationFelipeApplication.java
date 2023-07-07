c3673232c33086213eec1b2f4888c071df424509
Felipe Ferreira de Vasconcelos <fvasconcelos-@hotmail.com>
2023-07-07 20:21:09 -0300

    add project

---
+++ src/main/java/com/project/professorallocation/ProfessorAllocationFelipeApplication.java
@@ -0,0 +1,13 @@
+package com.project.professorallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationFelipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationFelipeApplication.class, args);
+	}
+
+}
